6ca81273fbac02624202a41f6349841c8ebe7c61
Jonathan Griffiths <jonathan@rosepip.com>
2018-09-02 22:12:56 +0100

    More editing tips.

--- edit_card.php
+++ edit_card.php
@@ -25,9 +25,9 @@ function EditCard($db, $cardId)
     echo "<form action=\"/flashcards/save_title.php/?cardId=", $cardId, "\" method=\"post\">";
     echo "<input type=\"text\" size=\"40\" name=\"title\" value=\"", $title, "\">";
     echo "<input type=\"submit\" value=\"Update\"></form>";
+    echo "(A blank title will remove the record from the database)<br><br>";
     echo "<form action=\"/flashcards/save_desc.php/?cardId=", $cardId, "\" method=\"post\">";
     echo "<textarea id=\"desc\" rows=\"10\" cols=\"100\" wrap=\"hard\" name=\"desc\">", $desc, "</textarea>";
-    #echo "<input type=\"textarea\" rows=\"10\" cols=\"80\" name=\"desc\" value=\"", $desc, "\">"; // Not quite right...
     echo "<input type=\"submit\" value=\"Update\">";
     echo "</form>";
     echo "Top tip - use HTML formatting for the description section<br><br>";
