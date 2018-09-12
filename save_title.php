aee4c62a935f63770fd718b9e11f17e680cd5789
Jonathan Griffiths <jonathan@rosepip.com>
2018-09-12 15:04:52 +0100

    Fixed small bug when creating new card and it said Card Not Found.  Also added more database entries

--- save_title.php
+++ save_title.php
@@ -11,17 +11,17 @@ $db = DatabaseInit();
 if ($db) {
     if ($cardId != -1) {
         if ($title != "") {
-            $query = "UPDATE cards SET title=\"".$title."\" WHERE card_id=".$cardId; # Update existing card
+            $db->exec("UPDATE cards SET title=\"".$title."\" WHERE card_id=".$cardId); # Update existing card
+            echo "<meta http-equiv=\"refresh\" content=\"0;url=/flashcards/ShowDesc.php\?cardId=",$cardId,"\"/>"; # Automatically re-display description
         } else {
-            $query = "DELETE FROM cards WHERE card_id=".$cardId;  # Remove empty card
+            $db->exec("DELETE FROM cards WHERE card_id=".$cardId);  # Remove empty card
+            echo "<meta http-equiv=\"refresh\" content=\"0;url=/flashcards/index.php\"/>"; # Automatically go back to index
         }
     } else {
-        $query = "INSERT INTO cards (title) VALUES(\"".$title."\")";  # Add new card
+        $db->exec("INSERT INTO cards (title) VALUES(\"".$title."\")");  # Add new card
+        echo "<meta http-equiv=\"refresh\" content=\"0;url=/flashcards/index.php\"/>"; # Automatically go back to index
     }
-    echo "Sending ",$query, " to DB<br>";
-    $count = $db->exec($query);
 }
-echo "<meta http-equiv=\"refresh\" content=\"0;url=/flashcards/ShowDesc.php\?cardId=",$cardId,"\"/>"; # Automatically re-display description
 echo "<a href=\"/flashcards/index.php\">Home</a>"; # Shouldn't be needed
 echo "</body></html>";
 ?>
